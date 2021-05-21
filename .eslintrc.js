e2bd84a484d023b0d46faebe407e09f661799741
oshimax <50955473+oshimax@users.noreply.github.com>
2021-05-21 18:05:16 +0900

    Initial setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,12 @@ module.exports = {
     'no-invalid-this': 'off',
     'no-var': 'warn',
     'require-jsdoc': 'off',
+    'semi': ['error', 'never', {'beforeStatementContinuationChars': 'never'}],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error'
   },
   'parserOptions': {
     'sourceType': 'module',
